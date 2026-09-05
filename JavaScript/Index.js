c23481a630d0183e971ca906c83f13f56af3bf83
Mirza Ismayeel <ismayeelmirza@gmail.com>
2026-09-05 23:09:29 +0430

    Improving some sidebar and add blur layer and creating offer section

--- JavaScript/Index.js
+++ JavaScript/Index.js
@@ -1,8 +1,10 @@
 let Sidebarclose = document.querySelector(".sidebar-close")
 let Navmenu = document.querySelector(".nav-menu")
 let Sidebar = document.querySelector(".sidebar-container")
+let blurlayer = document.querySelector(".blur-layer")
 let Sidebarauto = ()=>{
     Sidebar.classList.toggle('sidebar-container-open')
+    blurlayer.classList.toggle('blur-layer-open')
     document.body.classList.toggle('overflow-hidden')
 }
 Navmenu.addEventListener("click", Sidebarauto)
