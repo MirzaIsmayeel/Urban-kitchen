507f640948230589f5a0eafaabc4f8bbb7acde59
Mirza Ismayeel <ismayeelmirza@gmail.com>
2026-09-06 21:46:34 +0430

    add Ese close in nav slider and create full photo gallery and make it responsive also

--- JavaScript/Index.js
+++ JavaScript/Index.js
@@ -8,5 +8,12 @@ let Sidebarauto = ()=>{
     document.body.classList.toggle('overflow-hidden')
 }
 Navmenu.addEventListener("click", Sidebarauto)
+document.addEventListener("keydown", (event)=>{
+    if(event.key === "Escape"){
+        Sidebar.classList.remove('sidebar-container-open')
+        blurlayer.classList.remove('blur-layer-open')
+        document.body.classList.remove('overflow-hidden')
+    }
+})
 Sidebar.addEventListener("click", Sidebarauto)
 Sidebar.addEventListener("click", Sidebarauto)
